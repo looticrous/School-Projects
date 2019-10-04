0f045a2641ddb0f4055eebe9ff9d5619c049327f
David Blanchard <davidblanchard84@gmail.com>
2019-10-04 00:27:47 -0500

    Generic Credentials in PDO

--- BEAssignment5/PDO.php
+++ BEAssignment5/PDO.php
@@ -3,7 +3,7 @@ $pdo_options = [
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ];
 try {
-  $pdo = new PDO("mysql:host=localhost;dbname=bnbs;charset=utf8mb4", "bnb_admin", "Th3P@ssw0rd1sN0tS3cure!!", $pdo_options);
+  $pdo = new PDO("mysql:host=localhost;dbname=bnbs;charset=utf8mb4", "username", "password", $pdo_options);
 } catch (PDOException $e) {
 echo $e->getMessage();
 }
